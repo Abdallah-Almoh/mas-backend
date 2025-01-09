a17dc1923004c00ac88fde06934e408e31ad237e
Abdullah Almohamad <93150320+Abdallah-Almoh@users.noreply.github.com>
2025-01-09 13:27:28 +0300

    Delete server/api/v1 directory

--- server/api/v1/controllers/blockchain/routes.js
+++
@@ -1,7 +0,0 @@
-const Express = require("express");
-const controller = require("./controller");
-const auth = require("../../../../helper/auth");
-
-module.exports = Express.Router()
-  .use(auth.verifyToken)
-  .post("/withdraw", controller.withdraw)
